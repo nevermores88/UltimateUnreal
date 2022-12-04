28eaa7ab65c54b76fc3e1d1b6b33c1c5f8a388e6
DESKTOP-I1FA69H\jp <nevermores88@hotmail.com>
2022-12-05 00:35:06 +0900

    08 Combat  - 68 Enemy Combat

--- Source/MyProject/Enemy.cpp
+++ Source/MyProject/Enemy.cpp
@@ -5,6 +5,7 @@
 #include "Components/SphereComponent.h"
 #include "AIController.h"
 #include "Main.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 // Sets default values
 AEnemy::AEnemy()
@@ -84,10 +85,18 @@ void AEnemy::MoveToTarget(AMain* Target)
 	{
 		FAIMoveRequest MoveRequest;
 		MoveRequest.SetGoalActor(Target);
-		MoveRequest.SetAcceptanceRadius(5.0f);
+		MoveRequest.SetAcceptanceRadius(25.0f);
 
 		FNavPathSharedPtr NavPath;
 		AIController->MoveTo(MoveRequest, &NavPath);
+
+		/*auto PathPoints = NavPath->GetPathPoints();
+		for (auto Point : PathPoints)
+		{
+			FVector Location = Point.Location;
+
+			UKismetSystemLibrary::DrawDebugSphere(this, Location, 25.f, 8, FLinearColor::Red, 10.f, 1.5f);
+		}*/
 	}
 }
 
